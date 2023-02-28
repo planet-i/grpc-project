9cbc4ea03f4799e39867f46a09e8f540c8f7a079
chen.silin <chen.silin@datatom.com>
2023-02-28 15:30:09 +0800

    feat:添加panic-recovery一元拦截器

--- server/grpc.go
+++ server/grpc.go
@@ -1,8 +1,25 @@
 package server
 
-import "google.golang.org/grpc"
+import (
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpcRecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 func NewGRPC() *grpc.Server {
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(grpcMiddleware.ChainUnaryServer(
+			grpcRecovery.UnaryServerInterceptor(RecoveryInterceptor()),
+		)),
+	)
 	return server
+}
+
+// RecoveryInterceptor panic时返回Unknown错误吗
+func RecoveryInterceptor() grpcRecovery.Option {
+	return grpcRecovery.WithRecoveryHandler(func(p interface{}) (err error) {
+		return status.Errorf(codes.Unknown, "panic triggered: %v", p)
+	})
 }
