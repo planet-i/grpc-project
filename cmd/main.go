a9562b0d936b11fa5fabc0f10bdcca033f4260c9
chen.silin <chen.silin@datatom.com>
2023-03-01 11:52:49 +0800

    fix:添加注释

--- cmd/main.go
+++ cmd/main.go
@@ -10,12 +10,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	// 初始化服务、注入依赖等
 	server, err := initServer()
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	// 服务启动
 	if err = server.Start(); err != nil {
 		log.Fatal(err)
 	}
--- server/http.go
+++ server/http.go
@@ -15,6 +15,7 @@ func NewHttp() *http.ServeMux {
 	mux := http.NewServeMux()
 	// 添加swagger-ui
 	serveSwaggerUI(mux)
+	// 在swagger界面输入此模式来加载文件 /swagger/xxx.swagger.json
 	mux.HandleFunc("/swagger/", serveSwaggerFile)
 	return mux
 }
@@ -25,15 +26,13 @@ func serveSwaggerFile(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// 获取当前程序的工作目录
-	projectPath, err := os.Getwd()
+	// 获取项目中 .swagger.json 文件的绝对路径
+	projectPath, err := os.Getwd() // 获取当前程序的工作目录
 	if err != nil {
 		panic("exec path err")
 	}
-	// 获取 .swagger.json 文件名称
-	jsonFile := strings.TrimPrefix(r.URL.Path, "/swagger/")
-	// 获取项目中 .swagger.json 文件的绝对路径
-	filePath := path.Join(projectPath, "/api", jsonFile)
+	jsonFile := strings.TrimPrefix(r.URL.Path, "/swagger/") // 获取 .swagger.json 文件名称
+	filePath := path.Join(projectPath, "/api", jsonFile)    // 拼接项目中 .swagger.json 文件的路径
 	// http.ServeFile() 函数向客户端发送 .swagger.json 文件的绝对路径
 	http.ServeFile(w, r, filePath)
 }
@@ -44,6 +43,7 @@ func serveSwaggerUI(mux *http.ServeMux) {
 		AssetDir: swagger.AssetDir,
 		Prefix:   "third_party/swagger-ui",
 	})
+	// 通过http://host:port/swagger-ui/ 打开swagger界面
 	prefix := "/swagger-ui/"
 	mux.Handle(prefix, http.StripPrefix(prefix, fileServer))
 }
